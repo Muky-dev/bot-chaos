92ece23a0ef825fb147d05f8e5f886a27d451ee5
chaos <rootslowed@gmail.com>
2021-01-30 13:20:08 -0300

    command to show help

--- commands/help.js
+++ commands/help.js
@@ -13,9 +13,6 @@ module.exports = {
 			.addField('clear', 'clear chat')
 			.setImage("https://img.ibxk.com.br/2020/01/30/30021141299110.jpg?w=1120&h=420&mode=crop&scale=both")
 			
-
-
-
 		message.channel.send(embed);
 	}
 }
